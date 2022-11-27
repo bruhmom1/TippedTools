4a1ec63d0c40b72e04ccefc3b3e97576cfe9c942
bruhmom1 <loganhzart@gmail.com>
2022-11-27 00:06:29 -0600

    Added Everything I Need So Far

--- src/main/java/net/BRUH_MOM/tippedtools/item/Items.java
+++ src/main/java/net/BRUH_MOM/tippedtools/item/Items.java
@@ -11,66 +11,77 @@ public class Items {
     public static final DeferredRegister<net.minecraft.world.item.Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TippedTools.MOD_ID);
 
-    // Emerald Tools
+    // Emerald Tools | Attack Speed +.2
 
     public static final RegistryObject<Item> EMERALD_TIPPED_SWORD = ITEMS.register("emerald_tipped_sword",
-            () -> new SwordItem(ModTiers.EMERALD_SWORD, 2, -2.4f,
+            () -> new SwordItem(ModTiers.EMERALD_SWORD, 2, -2.2f, // attack speed
             new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
     public static final RegistryObject<Item> EMERALD_TIPPED_AXE = ITEMS.register("emerald_tipped_axe",
-            () -> new AxeItem(ModTiers.EMERALD_AXE, 2, -2.8f,
+            () -> new AxeItem(ModTiers.EMERALD_AXE, 2, -2.9f,
             new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<Item> EMERALD_TIPPED_PICKAXE = ITEMS.register("emerald_tipped_pickaxe",
-            () -> new PickaxeItem(ModTiers.EMERALD_PICKAXE, 2, -2.8f,
+            () -> new PickaxeItem(ModTiers.EMERALD_PICKAXE, 2, -2.6f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<Item> EMERALD_TIPPED_SHOVEL = ITEMS.register("emerald_tipped_shovel",
             () -> new ShovelItem(ModTiers.EMERALD_SHOVEL, 2, -2.8f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
-    public static final RegistryObject<Item> EMERALD_TIPPED_HOE = ITEMS.register("emerald_tipped_HOE",
-            () -> new HoeItem(ModTiers.EMERALD_HOE, 2, -2.8f,
+    public static final RegistryObject<Item> EMERALD_TIPPED_HOE = ITEMS.register("emerald_tipped_hoe",
+            () -> new HoeItem(ModTiers.EMERALD_HOE, 2, -0.8f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
 
-    // Diamond Tools
+    // Diamond Tools |
 
     public static final RegistryObject<Item> DIAMOND_TIPPED_SWORD = ITEMS.register("diamond_tipped_sword",
             () -> new SwordItem(ModTiers.DIAMOND_SWORD, 3, -2.4f,
                     new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
     public static final RegistryObject<Item> DIAMOND_TIPPED_AXE = ITEMS.register("diamond_tipped_axe",
-            () -> new AxeItem(ModTiers.DIAMOND_AXE, 3, -2.4f,
+            () -> new AxeItem(ModTiers.DIAMOND_AXE, 3, -3f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<Item> DIAMOND_TIPPED_PICKAXE = ITEMS.register("diamond_tipped_pickaxe",
-            () -> new PickaxeItem(ModTiers.DIAMOND_PICKAXE, 3, -2.4f,
+            () -> new PickaxeItem(ModTiers.DIAMOND_PICKAXE, 3, -2.8f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<Item> DIAMOND_TIPPED_SHOVEL = ITEMS.register("diamond_tipped_shovel",
-            () -> new ShovelItem(ModTiers.DIAMOND_SHOVEL, 3, -2.4f,
+            () -> new ShovelItem(ModTiers.DIAMOND_SHOVEL, 3, -3f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<Item> DIAMOND_TIPPED_HOE = ITEMS.register("diamond_tipped_hoe",
-            () -> new HoeItem(ModTiers.DIAMOND_HOE, 3, -2.4f,
+            () -> new HoeItem(ModTiers.DIAMOND_HOE, 3, -0f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
 
-    // Netherite Tools
+    // Netherite Tools |
 
     public static final RegistryObject<Item> NETHERITE_TIPPED_SWORD = ITEMS.register("netherite_tipped_sword",
-            () -> new SwordItem(ModTiers.NETHERITE_SWORD, 3, -2.4f,
+            () -> new SwordItem(ModTiers.NETHERITE_SWORD, 4, -2.4f,
                     new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
     public static final RegistryObject<Item> NETHERITE_TIPPED_AXE = ITEMS.register("netherite_tipped_axe",
-            () -> new AxeItem(ModTiers.NETHERITE_AXE, 3, -2.4f,
+            () -> new AxeItem(ModTiers.NETHERITE_AXE, 4, -3f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<Item> NETHERITE_TIPPED_PICKAXE = ITEMS.register("netherite_tipped_pickaxe",
-            () -> new PickaxeItem(ModTiers.NETHERITE_PICKAXE, 3, -2.4f,
+            () -> new PickaxeItem(ModTiers.NETHERITE_PICKAXE, 4, -2.8f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<Item> NETHERiTE_TIPPED_SHOVEL = ITEMS.register("netherite_tipped_shovel",
-            () -> new ShovelItem(ModTiers.NETHERITE_SHOVEL, 3, -2.4f,
+            () -> new ShovelItem(ModTiers.NETHERITE_SHOVEL, 4, -3f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static final RegistryObject<Item> NETHERITE_TIPPED_HOE = ITEMS.register("netherite_tipped_hoe",
-            () -> new HoeItem(ModTiers.NETHERITE_HOE, 3, -2.4f,
+            () -> new HoeItem(ModTiers.NETHERITE_HOE, 4, 0f,
                     new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
 
+    // Gold Tools | Attack Speed +.4
 
-
-
-
-
-
+    public static final  RegistryObject<Item> GOLD_TIPPED_SWORD = ITEMS.register("gold_tipped_sword",
+            () -> new SwordItem(ModTiers.GOLD_SWORD, 2, -2f,
+                    new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+    public static final  RegistryObject<Item> GOLD_TIPPED_AXE = ITEMS.register("gold_tipped_axe",
+            () -> new AxeItem(ModTiers.GOLD_AXE, 2, -2.7f,
+                    new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final  RegistryObject<Item> GOLD_TIPPED_PIACKAXE = ITEMS.register("gold_tipped_pickaxe",
+            () -> new PickaxeItem(ModTiers.GOLD_PICKAXE, 2, -2.4f,
+                    new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final  RegistryObject<Item> GOLD_TIPPED_SHOVEL = ITEMS.register("gold_tipped_shovel",
+            () -> new ShovelItem(ModTiers.GOLD_SHOVEL, 2, -2.6f,
+                    new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final  RegistryObject<Item> GOLD_TIPPED_HOE = ITEMS.register("gold_tipped_hoe",
+            () -> new HoeItem(ModTiers.GOLD_HOE, 2, -0.6f,
+                    new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
--- src/main/java/net/BRUH_MOM/tippedtools/item/ModTiers.java
+++ src/main/java/net/BRUH_MOM/tippedtools/item/ModTiers.java
@@ -7,61 +7,50 @@ import net.minecraftforge.common.ForgeTier;
 
 public class ModTiers {
 
-    // Emerald Tools
-    public static final ForgeTier EMERALD_SWORD = new ForgeTier(2, 374, 2f, 3f, 18, BlockTags.NEEDS_IRON_TOOL,
+    // Emerald Tools | Attack Damage = +.2 | Durability = Slightly More Than Iron Tools
+    public static final ForgeTier EMERALD_SWORD = new ForgeTier(2, 374, 6f, 3.2f, 32, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.EMERALD));
-    public static final ForgeTier EMERALD_AXE = new ForgeTier(2, 374, 6f, 4.5f, 18, BlockTags.NEEDS_IRON_TOOL,
+    public static final ForgeTier EMERALD_AXE = new ForgeTier(2, 374, 6f, 6.2f, 32, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.EMERALD));
-    public static final ForgeTier EMERALD_PICKAXE = new ForgeTier(2, 374, 2f, 3f, 18, BlockTags.NEEDS_IRON_TOOL,
+    public static final ForgeTier EMERALD_PICKAXE = new ForgeTier(2, 374, 6f, 1.2f, 32, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.EMERALD));
-    public static final ForgeTier EMERALD_SHOVEL = new ForgeTier(2, 374, 2f, 3f, 18, BlockTags.NEEDS_IRON_TOOL,
+    public static final ForgeTier EMERALD_SHOVEL = new ForgeTier(2, 374, 6f, 1.7f, 32, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.EMERALD));
-    public static final ForgeTier EMERALD_HOE = new ForgeTier(2, 374, 2f, 3f, 18, BlockTags.NEEDS_IRON_TOOL,
+    public static final ForgeTier EMERALD_HOE = new ForgeTier(2, 374, 6f, -1.8f, 32, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.EMERALD));
-    // Diamond Tools
-    public static final ForgeTier DIAMOND_SWORD = new ForgeTier(3, 770, 2f, 3f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
+    // Diamond Tools | Attack Damage = Diamond Tools | Durability = 1/3 Of Diamond Tools + Iron Tools
+    public static final ForgeTier DIAMOND_SWORD = new ForgeTier(3, 770, 8f, 3f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.DIAMOND));
-    public static final ForgeTier DIAMOND_AXE = new ForgeTier(3, 770, 2f, 3f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier DIAMOND_AXE = new ForgeTier(3, 770, 8f, 5f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.DIAMOND));
-    public static final ForgeTier DIAMOND_PICKAXE = new ForgeTier(3, 770, 2f, 3f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier DIAMOND_PICKAXE = new ForgeTier(3, 770, 8f, 1f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.DIAMOND));
-    public static final ForgeTier DIAMOND_SHOVEL = new ForgeTier(3, 770, 2f, 3f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier DIAMOND_SHOVEL = new ForgeTier(3, 770, 8f, 1.5f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.DIAMOND));
-    public static final ForgeTier DIAMOND_HOE = new ForgeTier(3, 770, 2f, 3f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier DIAMOND_HOE = new ForgeTier(3, 770, 8f, -3f, 14, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.DIAMOND));
-    // Netherite Tools
-    public static final ForgeTier NETHERITE_SWORD = new ForgeTier(4, 927, 2f, 3f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
+    // Netherite Tools | Attack Damage = Netherite Tools | Durability = 1/3 Of Netherite Tools + Iron Tools
+    public static final ForgeTier NETHERITE_SWORD = new ForgeTier(4, 927, 9f, 3f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.NETHERITE_SCRAP));
-    public static final ForgeTier NETHERITE_AXE = new ForgeTier(4, 927, 2f, 3f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier NETHERITE_AXE = new ForgeTier(4, 927, 9f, 5f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.NETHERITE_SCRAP));
-    public static final ForgeTier NETHERITE_PICKAXE = new ForgeTier(4, 927, 2f, 3f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier NETHERITE_PICKAXE = new ForgeTier(4, 927, 9f, 1f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.NETHERITE_SCRAP));
-    public static final ForgeTier NETHERITE_SHOVEL = new ForgeTier(4, 927, 2f, 3f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier NETHERITE_SHOVEL = new ForgeTier(4, 927, 9f, 1.5f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.NETHERITE_SCRAP));
-    public static final ForgeTier NETHERITE_HOE = new ForgeTier(4, 927, 2f, 3f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
+    public static final ForgeTier NETHERITE_HOE = new ForgeTier(4, 927, 9f, -4f, 15, BlockTags.NEEDS_DIAMOND_TOOL,
             () -> Ingredient.of(Items.NETHERITE_SCRAP));
-    // Gold Tools
-    public static final ForgeTier GOLD_SWORD = new ForgeTier(2, 282, 2f, 3f, 22, BlockTags.NEEDS_IRON_TOOL,
+    // Gold Tools | Attack Damage = +.1 | Durability = Iron + Gold Tools
+    public static final ForgeTier GOLD_SWORD = new ForgeTier(2, 282, 12f, 3.1f, 22, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.GOLD_INGOT));
-    public static final ForgeTier GOLD_AXE = new ForgeTier(2, 282, 2f, 3f, 22, BlockTags.NEEDS_IRON_TOOL,
+    public static final ForgeTier GOLD_AXE = new ForgeTier(2, 282, 12f, 6.1f, 22, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.GOLD_INGOT));
-    public static final ForgeTier GOLD_PICKAXE = new ForgeTier(2, 282, 2f, 3f, 22, BlockTags.NEEDS_IRON_TOOL,
+    public static final ForgeTier GOLD_PICKAXE = new ForgeTier(2, 282, 12f, 1.1f, 22, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.GOLD_INGOT));
-    public static final ForgeTier GOLD_SHOVEL = new ForgeTier(2, 282, 2f, 3f, 22, BlockTags.NEEDS_IRON_TOOL,
+    public static final ForgeTier GOLD_SHOVEL = new ForgeTier(2, 282, 12f, 1.6f, 22, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.GOLD_INGOT));
-    public static final ForgeTier GOLD_HOE = new ForgeTier(2, 282, 2f, 3f, 22, BlockTags.NEEDS_IRON_TOOL,
+    public static final ForgeTier GOLD_HOE = new ForgeTier(2, 282, 12f, -1.9f, 22, BlockTags.NEEDS_IRON_TOOL,
             () -> Ingredient.of(Items.GOLD_INGOT));
-    // Lapis Tools
-    public static final ForgeTier LAPIS_SWORD = new ForgeTier(2, 374, 2f, 3f, 32, BlockTags.NEEDS_IRON_TOOL,
-            () -> Ingredient.of(Items.LAPIS_LAZULI));
-    public static final ForgeTier LAPIS_AXE = new ForgeTier(2, 374, 2f, 3f, 32, BlockTags.NEEDS_IRON_TOOL,
-            () -> Ingredient.of(Items.LAPIS_LAZULI));
-    public static final ForgeTier LAPIS_PICKAXE = new ForgeTier(2, 374, 2f, 3f, 32, BlockTags.NEEDS_IRON_TOOL,
-            () -> Ingredient.of(Items.LAPIS_LAZULI));
-    public static final ForgeTier LAPIS_SHOVEL = new ForgeTier(2, 374, 2f, 3f, 32, BlockTags.NEEDS_IRON_TOOL,
-            () -> Ingredient.of(Items.LAPIS_LAZULI));
-    public static final ForgeTier lAPIS_HOE = new ForgeTier(2, 374, 2f, 3f, 32, BlockTags.NEEDS_IRON_TOOL,
-            () -> Ingredient.of(Items.LAPIS_LAZULI));
 
 }
 
