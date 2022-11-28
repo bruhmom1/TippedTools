adfcfedc33453046754eb82afca3af08f44a7ddd
bruhmom1 <loganhzart@gmail.com>
2022-11-28 11:16:51 -0600

    Initial commit

---
+++ src/main/java/net/BRUH_MOM/tippedtools/Client.java
@@ -0,0 +1,10 @@
+package net.BRUH_MOM.tippedtools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/BRUH_MOM/tippedtools/TippedTools.java
@@ -0,0 +1,15 @@
+package net.BRUH_MOM.tippedtools;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TippedTools implements ModInitializer {
+	public static final String MOD_ID = "tippedtools";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
